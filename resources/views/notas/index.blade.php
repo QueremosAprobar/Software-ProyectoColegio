da967a2c2eae9bc91447a2464b424083fc16b95f
Johan Jef <091609@unsaac.edu.pe>
2018-01-26 00:26:35 -0500

    modficando vista Notas

--- resources/views/notas/index.blade.php
+++ resources/views/notas/index.blade.php
@@ -11,14 +11,46 @@
 
 @stop
 
+<script type="text/javascript">
+/* Sumar dos números. */
+function promediar (valor) {
+    var total = 0;  
+    valor = parseInt(valor); // Convertir el valor a un entero (número).
+    
+    total = document.getElementById('spTotal').innerHTML;
+    
+    // Aquí valido si hay un valor previo, si no hay datos, le pongo un cero "0".
+    total = (total == null || total == undefined || total == "") ? 0 : total;
+    
+    /* Esta es la suma. */
+    total = (parseInt(total) + parseInt(valor));
+    
+    // Colocar el resultado de la suma en el control "span".
+    document.getElementById('spTotal').innerHTML = total;
+}
+
+function p(){
+var num1 = parseFloat(document.getElementById('n1').value);
+var num2 =parseFloat(document.getElementById('n2').value);
+var num3 = parseFloat(document.getElementById('n3').value);
+var pro =(num1+num2+num3)/3;
+if(!isNaN(pro))
+document.getElementById('promedio').innerHTML = pro.toFixed(2);
+}
+window.onload = p;
+</script>
+
 @section('content')
 
     <div class="row">
         <div class="col-lg-12">
-            <h3 class="page-header">Notas
-                <button type="button" class="btn btn-primary" onclick="location.href='Notas/create'">
-                    Nuevo
-                </button>
+            <h3 class="page-header">Seleccione su Curso
+                <SELECT NAME="selCombo" SIZE=1 onChange="javascript:alert('prueba');"> 
+                    <OPTION VALUE="1">Link1</OPTION>
+                    <OPTION VALUE="2">Link2</OPTION>
+                    <OPTION VALUE="3">Link3</OPTION>
+                    <OPTION VALUE="4">Link4</OPTION> 
+                </SELECT>
             </h3>
         </div>
         <!-- /.col-lg-12 -->
@@ -58,7 +90,7 @@
                                     <th>p2</th>
                                     <th>p3</th>
                                     <th>promedio</th>
-                                    <th>Operaciones</th>
+                                    <th>Observaciones</th>
                                 </tr>
                                 </thead>
 
@@ -67,12 +99,34 @@
 
                                     <tr class="odd gradeA" rol="row">
                                         <td>{{ $Nota->idcurso }}</td>
-                                        <td>{{ $Nota->dnialumno }}</td>
-                                        <td>{{ $Nota->p1 }}</td>
-                                        <td>{{ $Nota->p2 }}</td>
-                                        <td>{{ $Nota->p3 }}</td>
-                                        <td>{{ $Nota->promedio }}</td>
-                                        
+                                        <td>{{ $Nota->dnialumno }}</td>                                    
+                                        <td>
+                                           <input type="number"  id="n1" onchange="p();" min="0.00"
+                                                max="20.00" step=".01" value={{ $Nota->p1 }} />
+                                            <a title="Editar" >
+                                                <span class="glyphicon glyphicon-pencil"></span>                                                
+                                            </a>
+                                        </td>
+                                        <td>
+                                            <input type="number"  id="n2" onchange="p();" min="0.00"
+                                                max="20.00" step=".01" value={{ $Nota->p2 }} />
+                                            <a title="Editar" >
+                                                <span class="glyphicon glyphicon-pencil"></span>                                                
+                                            </a>                                          
+                                        </td>
+                                        <td>
+                                            <input type="number"  id="n3" onchange="p();" min="0.00"
+                                                max="20.00" step=".01" value={{ $Nota->p3 }} />
+                                            <a title="Editar" >
+                                                <span class="glyphicon glyphicon-pencil"></span>                                                
+                                            </a>  
+                                        </td>
+                                        <td><span id="promedio"></span></td>
+                                        <td> 
+                                            @if( ($Nota->p3 + $Nota->p2 + $Nota->p1)/3 > 10 )
+                                                <p style="color:red;">Aprobado</p> 
+                                            @else <p style="color:red;">Desaprobado</p> @endif
+                                        </td>                                      
                                     </tr>
                                 @endforeach
                                 </tbody>
@@ -86,6 +140,22 @@
         </div>
 
     </div>
+    <button type="button" class="btn btn-primary" onclick="location.href='Notas/create'">
+                    Nuevo
+    </button>
+
+<form action="../../form-result.php" method="post" target="_blank">
+
+  <p>
+
+    Limones usados para el jugo: <input type="number" name="limonesusados" min="1" max="5" step="0.5">
+
+    <input type="submit" class="btn btn-primary" value="Enviar datos">
+
+  </p>
+
+</form>
+
 
 @stop
 
